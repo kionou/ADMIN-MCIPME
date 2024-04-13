1bafc10fe95127e3d37d3242dc4349782d323770
DiarafaSoukouna <diarafasouk@gmail.com>
2024-04-13 10:03:03 +0000

    Nouvelle mise a jour

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,87 +1,81 @@
-
 export default [
-
   // To start account
   {
     path: "/",
     name: "login",
     component: () => import("../views/account/login.vue"),
-   
   },
   {
     path: "/reinitialiser",
     name: "reinitialiser",
-    component: () => import("../views/account/recoverpw-sample.vue")
+    component: () => import("../views/account/recoverpw-sample.vue"),
   },
 
-   // To finish account
+  // To finish account
 
-    // To start dashboards
+  // To start dashboards
 
   {
     path: "/dncic",
     name: "Dncic",
     meta: { requiresAuth: true },
-    component: () => import("../views/dashboards/dncic.vue")
+    component: () => import("../views/dashboards/dncic.vue"),
   },
   {
     path: "/dni",
     name: "Dni",
     meta: { requiresAuth: true },
-    component: () => import("../views/dashboards/default.vue")
+    component: () => import("../views/dashboards/default.vue"),
   },
 
- // To finish dashboards
-
+  // To finish dashboards
 
   // To start users
   {
     path: "/utilisateurs",
     name: "users",
     meta: { requiresAuth: true },
-    component: () => import("../views/users/index.vue")
+    component: () => import("../views/users/index.vue"),
   },
-  
 
- // To start partenaires
+  // To start partenaires
 
- {
-  path: "/partenaires",
-  name: "partenaires",
-  meta: { requiresAuth: true },
-  component: () => import("../views/partenaires/defaut.vue")
-},
-{
-  path: "/partenaires/ajouter",
-  name: "partenaire-add",
-  meta: { requiresAuth: true },
-  component: () => import("../views/partenaires/ajouter.vue")
-},
-{
-  path: "/partenaires/update/:id",
-  name: "partenaire-put",
-  props:true,
-  meta: { requiresAuth: true },
-  component: () => import("../views/partenaires/update.vue")
-},
-// To finish partenaires
+  {
+    path: "/partenaires",
+    name: "partenaires",
+    meta: { requiresAuth: true },
+    component: () => import("../views/partenaires/defaut.vue"),
+  },
+  {
+    path: "/partenaires/ajouter",
+    name: "partenaire-add",
+    meta: { requiresAuth: true },
+    component: () => import("../views/partenaires/ajouter.vue"),
+  },
+  {
+    path: "/partenaires/update/:id",
+    name: "partenaire-put",
+    props: true,
+    meta: { requiresAuth: true },
+    component: () => import("../views/partenaires/update.vue"),
+  },
+  // To finish partenaires
 
- // To start pme
+  // To start pme
 
- {
-  path: "/industrie",
-  name: "industrie",
-  meta: { requiresAuth: true },
-  component: () => import("../views/pme/default.vue")
-},
-// {
-//   path: "/pme/ajouter",
-//   name: "partenaire-add",
-//   meta: { requiresAuth: true },
-//   component: () => import("../views/pme/ajouter.vue")
-// },
-// To finish pme
-
+  {
+    path: "/industrie",
+    name: "industrie",
+    meta: { requiresAuth: true },
+    component: () => import("../views/pme/default.vue"),
+  },
+  // {
+  //   path: "/pme/ajouter",
+  //   name: "partenaire-add",
+  //   meta: { requiresAuth: true },
+  //   component: () => import("../views/pme/ajouter.vue")
+  // },
+  // To finish pme
 
   // To start localites
 
@@ -89,85 +83,76 @@ export default [
     path: "/localites",
     name: "localites",
     meta: { requiresAuth: true },
-    component: () => import("../views/localites/default.vue")
+    component: () => import("../views/localites/default.vue"),
   },
- 
 
- // To finish localites
+  // To finish localites
 
- 
- // To start secteur
+  // To start secteur
 
- {
-  path: "/secteurs",
-  name: "secteurs",
-  meta: { requiresAuth: true },
-  component: () => import("../views/secteurs/default.vue")
-},
+  {
+    path: "/secteurs",
+    name: "secteurs",
+    meta: { requiresAuth: true },
+    component: () => import("../views/secteurs/default.vue"),
+  },
 
+  // To start archive
 
- 
+  {
+    path: "/archive",
+    name: "archive",
+    meta: { requiresAuth: true },
+    component: () => import("../views/archives/default.vue"),
+  },
+  {
+    path: "/archive/detail/:id",
+    name: "archive-detail",
+    meta: { requiresAuth: true },
+    props: true,
+    component: () => import("../views/archives/detail.vue"),
+  },
 
- // To start archive
+  // To start documents
 
- {
-  path: "/archive",
-  name: "archive",
-  meta: { requiresAuth: true },
-  component: () => import("../views/archives/default.vue")
-},
-{
-  path: "/archive/detail/:id",
-  name: "archive-detail",
-  meta: { requiresAuth: true },
-  props:true,
-  component: () => import("../views/archives/detail.vue")
-},
+  {
+    path: "/documents",
+    name: "documents",
+    meta: { requiresAuth: true },
+    component: () => import("../views/documents/default.vue"),
+  },
 
-// To start documents
+  // To start juridique
 
-{
-  path: "/documents",
-  name: "documents",
-  meta: { requiresAuth: true },
-  component: () => import("../views/documents/default.vue")
-},
+  {
+    path: "/status-juridique",
+    name: "status-juridique",
+    meta: { requiresAuth: true },
+    component: () => import("../views/juridique/default.vue"),
+  },
 
+  // To start zone industrielle
 
- // To start juridique
+  {
+    path: "/zone-industrielle",
+    name: "zone-industrielle",
+    meta: { requiresAuth: true },
+    component: () => import("../views/zone/default.vue"),
+  },
 
- {
-  path: "/status-juridique",
-  name: "status-juridique",
-  meta: { requiresAuth: true },
-  component: () => import("../views/juridique/default.vue")
-},
+  // To start zone indicateurs
 
+  {
+    path: "/indicateurs",
+    name: "indicateurs",
+    meta: { requiresAuth: true },
+    component: () => import("../views/indicateurs/default.vue"),
+  },
 
- // To start zone industrielle
-
- {
-  path: "/zone-industrielle",
-  name: "zone-industrielle",
-  meta: { requiresAuth: true },
-  component: () => import("../views/zone/default.vue")
-},
- 
-// To start zone indicateurs
-
-{
-  path: "/indicateurs",
-  name: "indicateurs",
-  meta: { requiresAuth: true },
-  component: () => import("../views/indicateurs/default.vue")
-},
- 
- 
-  
-  
- 
- 
-  
-  
-
+  {
+    path: "/type",
+    name: "type",
+    meta: { requiresAuth: true },
+    component: () => import("../views/demandes/type.vue"),
+  },
 ];
